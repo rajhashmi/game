c86605249dbecc7ef025a3799d7890e6a69c19bd
shahid <shahidhashmiraj@gmail.com>
2024-11-18 16:41:59 +0530

    Fix for serverless Vercel deployment

--- game-server/api/index.js
+++ game-server/api/index.js
@@ -1,27 +1,29 @@
 const express = require("express");
-const http = require('http');
+const http = require("http");
 const { Server } = require("socket.io");
-const app = express();
-const server = http.createServer(app)
 const cors = require("cors");
 
-app.use(cors({
-    origin: "https://multiplayer-game-frontend.vercel.app",  
-    methods: ["GET", "POST"]
-}));
+const app = express();
+const server = http.createServer(app);
+
+app.use(
+  cors({
+    origin: "https://multiplayer-game-frontend.vercel.app",
+    methods: ["GET", "POST"],
+  })
+);
 
 const io = new Server(server, {
-    cors: {
-        origin : 'https://multiplayer-game-frontend.vercel.app',
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: "https://multiplayer-game-frontend.vercel.app",
+    methods: ["GET", "POST"],
+  },
 });
 
-
 io.on("connection", (socket) => {
-    console.log("A user connected:", socket.id);
-})
+  console.log("A user connected:", socket.id);
+});
 
-app.get("/", (req, res) => res.json({message: "this is vercel server"}));
+app.get("/", (req, res) => res.json({ message: "this is vercel server" }));
 
-server.listen(3000, () => console.log("Server ready on port 3000"));
+module.exports = app;
