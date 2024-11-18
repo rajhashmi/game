d8671e6027b3b9d5a195f78ca533cf0e139cb3b8
shahid <shahidhashmiraj@gmail.com>
2024-11-18 14:25:38 +0530

    made changes in vercel

--- backend/Index.js
+++ backend/Index.js
@@ -5,20 +5,19 @@ const setupWebSocket = require('./WebSocket');
 const PORT = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
+
 const corsOptions = {
     origin: 'https://multiplayer-game-frontend.vercel.app', 
     methods: ['GET', 'POST'],
     credentials: true,
 };
 
-// Middleware
 app.use(cors(corsOptions));
 
-app.get('/hello', (req, res) => {
-    res.send('<h1>Hello</h1>');
+app.get('/', (req, res) => {
+    res.json({message: "this is mesage from server"});
 });
 
-// Initialize WebSocket
 setupWebSocket(server);  
 
 server.listen(PORT, () => {
