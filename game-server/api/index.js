04c9ed73e5a5b0f7cc5e78e1e089cb43a80edf45
shahid <shahidhashmiraj@gmail.com>
2024-11-18 16:27:54 +0530

    added test socket

--- game-server/api/index.js
+++ game-server/api/index.js
@@ -1,6 +1,27 @@
 const express = require("express");
+const http = require('http');
+const { Server } = require("socket.io");
 const app = express();
+const server = http.createServer(app)
+const cors = require("cors");
+
+app.use(cors({
+    origin: "https://multiplayer-game-frontend.vercel.app",  
+    methods: ["GET", "POST"]
+}));
+
+const io = new Server(server, {
+    cors: {
+        origin : 'https://multiplayer-game-frontend.vercel.app',
+        methods: ["GET", "POST"]
+    }
+});
+
+
+io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+})
 
 app.get("/", (req, res) => res.json({message: "this is vercel server"}));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+server.listen(3000, () => console.log("Server ready on port 3000"));
